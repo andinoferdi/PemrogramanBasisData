42fb10d8be53ddd0da59bf42fbd924b09c05f086
andinoferdi <andinoferdiansah@gmail.com>
2024-11-26 08:39:32 +0700

    update

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,20 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Role extends Model
+class Role extends Authenticatable
 {
-    use HasFactory;
-
     protected $table = 'role';
 
     protected $primaryKey = 'role_id';
 
-    protected $fillable = [
-        'nama_role'
-    ];
 
     public $timestamps = false;
+
+    
 }
